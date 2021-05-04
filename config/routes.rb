4c6e23e21eab42cf30946d2dce27819ffc374932
Toshita Pandey <toshitapandey@gmail.com>
2021-05-05 03:58:57 +0530

    add user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  namespace :api, defaults: { format: :json } do
+    # GET /api/users - returns all users from users table
+    get 'users' => 'users#index'
+
+    # GET /api/user/:id - returns specific user from users table given id
+    get 'user' => 'users#show'
+
+    # POST /api/user - adds a new user to the users table
+    post 'user' => 'users#update'
+
+    # PUT /api/user/:id - updates specific user in users table
+    put 'user' => 'users#update'
+
+    # DELETE /api/user/:id - removes the specific user from users table with the given id
+    delete 'user' => 'users#delete'
+  end
 end
